84ee725115f8426a8be681cba2ae341978c131a2
Maneesha743527 <149286663+Maneesha743527@users.noreply.github.com>
2024-03-21 21:00:14 +0530

    Add files via upload

---
+++ Ifexam.java
@@ -0,0 +1,11 @@
+class Ifexam{
+  public static void main(String args[]){
+  int marks=70;
+  if(marks>60){
+   System.out.println("you are failed in the exam");
+}
+else{
+  System.out.println("you have passed the exam");
+}
+}
+}
