d125dc6bfa44ef86c47f4b47ff47a0024034e957
Maneesha743527 <149286663+Maneesha743527@users.noreply.github.com>
2024-04-06 08:12:25 +0530

    Add files via upload

--- StudentImp.java
+++ StudentImp.java
@@ -1,16 +1,19 @@
 class StudentImp{
   public static void main(String args[]){
-  Student s= new Student("james","123");
-  s.display();
+     Student s = new Student("james","1232");
+     s.display();
+  }
 }
-}
+
 class Student{
-String nam,rn;
-Student(String name,String rno){
-nam = name;
-rn = rno;
-}
-public void display(){
-System.out.printf("Welcome %s rno is %s\n",nam,rn);
-}
+     String nam,rn;
+
+
+     Student(String name,String rno){
+         nam = name;
+         rn = rno;
+  }
+     public void display(){
+        System.out.printf("welcome %s rno is %s\n",nam,rn);
+  }
 }
