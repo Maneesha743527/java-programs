06d072a99361dca725adb536acddd7eda8fe7cc9
Maneesha743527 <149286663+Maneesha743527@users.noreply.github.com>
2024-04-06 08:11:17 +0530

    Add files via upload

---
+++ ComArray.java
@@ -0,0 +1,29 @@
+public class ComArray{
+int [] arr3 = {4,7,3,9,2};
+public static void main(String args[]){
+int[] arr1 = {4,7,3,9,2};
+int[] arr2 = {3,2,12,9,40,32,4};
+for(int i=0;i<arr1.length;i++){
+ if(arr1[i]==arr2[j]){
+System.out.println(arr1[i]);
+}
+}
+}
+}
+
+class IterateArray{
+  public static void main(string arg[]){
+int[] arr = {1,2,4,5,6,7};
+ComArray co = new ComArray();
+for(int l:co.arr3){
+System.out.println(l);
+}
+ComArray.main(null);
+System.out.println("elements are");
+for(int i=0;i<arr.length;i++){
+if(arr[i]==arr2[j]){
+System.out.println(arr[i]);
+}
+}
+}
+}
