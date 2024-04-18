e666c61f3875990e36c41748366a95a37ce450f9
Maneesha743527 <149286663+Maneesha743527@users.noreply.github.com>
2024-04-18 13:11:23 +0530

    Add files via upload

---
+++ Concat.java
@@ -0,0 +1,11 @@
+class Concat{
+public static void main(String args[]){
+String s1="rahul";
+System.out.println(s1.replace('R','r'));
+System.out.println(s1.length());
+System.out.println(s1.toLowerCase());
+System.out.println(s1.toUpperCase());
+
+
+}
+}
