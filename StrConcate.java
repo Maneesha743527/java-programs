347fbf8689a49801d1465268470e23b5279f6721
Maneesha743527 <149286663+Maneesha743527@users.noreply.github.com>
2024-04-18 13:10:29 +0530

    Add files via upload

---
+++ StrConcate.java
@@ -0,0 +1,9 @@
+class StrConcate{
+ public static void main(String srgs[]){
+    String s1="maneesha";
+    String s2="maneesha";
+    String s3=new String("maneesha");
+    System.out.println(s1==s2);
+    System.out.println(s1==s3);
+}
+}
