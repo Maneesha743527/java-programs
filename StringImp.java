cb841c8d9839c8401260956d4375b99340324f82
Maneesha743527 <149286663+Maneesha743527@users.noreply.github.com>
2024-04-18 13:08:39 +0530

    Add files via upload

---
+++ StringImp.java
@@ -0,0 +1,12 @@
+class StringImp{
+  public static void main(String args[]){
+String str1="Hello";
+String  str2=new String(str1);
+System.out.println("styring1 is:"+str1);
+System.out.println("string2 is:"+str2);
+str2=str1.concat("world");
+System.out.println("string1 is:"+str1);
+System.out.println("modified string is:"+str2);
+}
+}
+
