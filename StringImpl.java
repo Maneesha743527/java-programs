82c6101330e3d3c451b9a3cffce7977d4995d6fc
Maneesha743527 <149286663+Maneesha743527@users.noreply.github.com>
2024-04-18 13:09:07 +0530

    Add files via upload

---
+++ StringImpl.java
@@ -0,0 +1,9 @@
+class StringImpl{
+      public static void main(String args[]){
+  
+String str1="Hello";
+String  str2=new String(str1);
+System.out.println("styring1 is:"+str1);
+System.out.println("string2 is:"+str2);
+}
+}
