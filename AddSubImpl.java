c6f72e31622f9c8acf3956aa0309b6d9b80bd566
Maneesha743527 <149286663+Maneesha743527@users.noreply.github.com>
2024-04-25 13:05:58 +0530

    Add files via upload

---
+++ AddSubImpl.java
@@ -0,0 +1,9 @@
+import  arithmeticop.*;
+class AddSubImpl{
+public static void main(String[] ARGS){
+Addition1 a1=new Addition1(4,3);
+a1.sum();
+Subtraction1 s1=new Subtraction1(5,4);
+s1.sub();
+}
+}
